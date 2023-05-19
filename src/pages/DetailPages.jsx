cc64c81f7abb79d204f8c8f4d87efb6977c57040
kagishokekanae <kagishokekana034@gmail.com>
2023-05-19 11:47:25 +0200

    Link added

--- src/pages/DetailPages.jsx
+++ src/pages/DetailPages.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Button from 'react-bootstrap/Button';
+
 
 
 function DetailPages() {
   return (
     <div>
     <Navbar />
-    DetailPages
-
     
+      <h1>
+      Your streaming guide for movies, TV shows & sports
+      </h1>
+
+      <p>Find where to stream new, popular & upcoming entertainment with JustWatch.</p>
+
+      <>
+      <Button href="/">Discovery Movies & TV Show</Button> <Button type="submit">Features</Button>{' '}
+    
+
+    </>
     </div>
   )
 }
