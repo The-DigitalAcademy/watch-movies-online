0af6836fcc7ff2d766bbe6bc8cc55e4b90dd8707
kagishokekanae <kagishokekana034@gmail.com>
2023-05-23 14:01:43 +0200

    popular

--- src/pages/SpecialPages.jsx
+++ src/pages/SpecialPages.jsx
@@ -35,13 +35,388 @@ function SpecialPages() {
       <Card.Body>
       </Card.Body>
     </Card></Col>
-    
-        <Col>2 of 2</Col>
+
+        <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://media.comicbook.com/2017/10/iron-man-movie-poster-marvel-cinematic-universe-1038878.jpg" />
+            </Card>
+            </Col>
       </Row>
+
       <Row>
-        <Col>1 of 3</Col>
-        <Col>2 of 3</Col>
-        <Col>3 of 3</Col>
+        <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://cdn.shopify.com/s/files/1/2356/1293/products/TheDarkKnight_800x.jpg?v=1655785830" /> 
+            </Card>
+            </Col>
+
+
+        <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://c4.wallpaperflare.com/wallpaper/764/590/391/inception-leonardo-dicaprio-movie-posters-2400x3500-entertainment-movies-hd-art-wallpaper-preview.jpg" /> 
+            </Card>
+            </Col>
+
+
+        <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://cdn.shopify.com/s/files/1/0057/3728/3618/products/wandavision.R85648_240x360_crop_center.progressive.jpg?v=1673638271" />
+            </Card>
+            </Col>
+
+            <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://cdn.shopify.com/s/files/1/0057/3728/3618/products/9d8e73e436b536a7c81644c6e9877c7a_1c9d0f90-9991-4326-8f37-3dd980abeacf_240x360_crop_center.progressive.jpg?v=1573590262" />
+            </Card>
+            </Col>
+
+            <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://img.fruugo.com/product/2/97/14401972_max.jpg" />
+            </Card>
+            </Col>
+
+            <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://www.joblo.com/wp-content/uploads/2020/01/the-flash-new-poster-3-400x600.jpg" />
+            </Card>
+            </Col>
+
+
+            <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://lwlies.com/wp-content/uploads/2020/04/rainman-poster-edited-900x0-c-default.jpg" />
+            </Card>
+            </Col>
+
+            <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://lionsgate.brightspotcdn.com/cc/c7/2e701ac14a22a2e3d81e292b3c80/john-wick-chapter-2-movies-poster-01.jpg " />
+            </Card>
+            </Col>
+
+            <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://www.joblo.com/wp-content/uploads/2022/10/the-equalizer-3-400x600.webp" />
+            </Card>
+            </Col>
+
+            <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://d994l96tlvogv.cloudfront.net/uploads/film/poster/031-kandahar-keyart-vertical-srgb-v4-1.jpg" />
+            </Card>
+            </Col>
+
+            <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://themarketingbirds.com/wp-content/uploads/2021/04/Creative-Movie-Posters-64-2.jpg" />
+            </Card>
+            </Col>
+
+
+            <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://lwlies.com/wp-content/uploads/2020/04/rainman-poster-edited-900x0-c-default.jpg" />
+            </Card>
+            </Col>
+
+            <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://lumiere-a.akamaihd.net/v1/images/p_disneymovies_avatarthewayofwater_1710_b7d39b03.jpeg" />
+            </Card>
+            </Col>
+
+            <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://amc-theatres-res.cloudinary.com/v1673700256/amc-cdn/production/2/movies/56500/56467/Poster/Primary_BoxCover_HD_800_1200.jpg" />
+            </Card>
+            </Col>
+
+            <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://m.media-amazon.com/images/M/MV5BOTBkZGJjNmMtNmE2YS00Y2M1LWE2ZDUtMTUyOTRlY2U2ZWJhXkEyXkFqcGdeQXVyMTA4NjE0NjEy._V1_.jpg" />
+            </Card>
+            </Col>
+
+
+            <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://www.joblo.com/wp-content/uploads/2022/06/hunger-games-poster-400x600.jpg" />
+            </Card>
+            </Col>
+
+            <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://i.pinimg.com/736x/e1/d9/7b/e1d97b207741ba60f2b6d2ee70f53915--poster-movies.jpg" />
+            </Card>
+            </Col>
+
+            <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://www.fortressofsolitude.co.za/wp-content/uploads/2022/03/Morbius-Poster.jpeg" />
+            </Card>
+            </Col>
+
+            <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://posterhouse.org/wp-content/uploads/2021/05/silence_of_the_lambs_0.jpg" />
+            </Card>
+            </Col>
+
+            <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://www.thepoke.co.uk/wp-content/uploads/2013/09/BT6c4dSIMAAnLU3.jpg" />
+            </Card>
+            </Col>
+
+            <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://cdn.shopify.com/s/files/1/0290/5663/0868/products/aviator_opt.webp?v=1668264196&width=533" />
+            </Card>
+            </Col>
+
+            <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://m.media-amazon.com/images/I/91-rkATc5iL._AC_SL1500_.jpg" />
+            </Card>
+            </Col>
+
+            <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://www.al.com/resizer/V6n0QmpSDHibsbgU_0gy1RG142U=/1280x0/smart/advancelocal-adapter-image-uploads.s3.amazonaws.com/image.al.com/home/bama-media/width2048/img/entertainment_impact/photo/brothers-grimsbyjpg-c1e326cf67da8143.jpg" />
+            </Card>
+            </Col>
+
+
+            <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://i.pinimg.com/originals/e2/0a/92/e20a9286a9c6327a663f051abd5b39e3.jpg" />
+            </Card>
+            </Col>
+
+            <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://assets-prd.ignimgs.com/2022/05/03/interceptor-en-us-main-vertical-1651614637898.jpg" />
+            </Card>
+            </Col>
+
+            <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://m.media-amazon.com/images/I/713+JsN7ZjL._SY550_.jpg" />
+            </Card>
+            </Col>
+
+            <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://www.vintagemovieposters.co.uk/wp-content/uploads/2020/11/IMG_5878-scaled.jpeg" />
+            </Card>
+            </Col>
+
+
+            <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/c7b71f16000235.56299c6499e04.png" />
+            </Card>
+            </Col>
+
+            <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://cdn.moviefone.com/admin-uploads/posters/codenamebanshee-movie-poster_1655423989.jpg?d=360x540&q=60" />
+            </Card>
+            </Col>
+
+            <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://img.fruugo.com/product/8/44/14237448_max.jpg" />
+            </Card>
+            </Col>
+
+            <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://www.etonline.com/sites/default/files/styles/970xh/public/images/2014-11/425_xmen_days.jpg?itok=ZMeMcR-T" />
+            </Card>
+            </Col>
+
+            <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://www.joblo.com/wp-content/uploads/2023/05/EN-US_EXTR2_Main_Train_Vertical_27x40_RGB_PRE-1-400x600.jpg" />
+            </Card>
+            </Col>
+
+            <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://hips.hearstapps.com/hmg-prod/images/his-father-s-legacy-1510255180.jpg?resize=480:*" />
+            </Card>
+            </Col>
+
+            <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://cdn.moviefone.com/admin-uploads/posters/assassinclub-movie-poster_1683328910.jpg?d=360x540&q=60" />
+            </Card>
+            </Col>
+
+            <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://moviesfilmsandflix.files.wordpress.com/2013/01/the-last-stand-arnold-movie-poster.jpg" />
+            </Card>
+            </Col>
+
+            <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://www.al.com/resizer/uP33kwD5rQy_Ij8Ew5ezhqq5-iE=/500x0/smart/advancelocal-adapter-image-uploads.s3.amazonaws.com/image.al.com/home/bama-media/width2048/img/entertainment_impact/photo/20563857-standard.jpg" />
+            </Card>
+            </Col>
+
+            <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1NRP7_i0mhatpVRisdCmF3uhu8XReRLVxwO7AYZxm8bhXWJqqvv4iGZL1Oe03-SPP7p8&usqp=CAU" />
+            </Card>
+            </Col>
+
+            <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="" />
+            </Card>
+            </Col>
+
+            <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://cdn.shopify.com/s/files/1/0057/3728/3618/products/wandavision.R85648_240x360_crop_center.progressive.jpg?v=1673638271" />
+            </Card>
+            </Col>
+
+            <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://cdn.shopify.com/s/files/1/0057/3728/3618/products/wandavision.R85648_240x360_crop_center.progressive.jpg?v=1673638271" />
+            </Card>
+            </Col>
+
+            <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://cdn.shopify.com/s/files/1/0057/3728/3618/products/wandavision.R85648_240x360_crop_center.progressive.jpg?v=1673638271" />
+            </Card>
+            </Col>
+
+            <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://cdn.shopify.com/s/files/1/0057/3728/3618/products/wandavision.R85648_240x360_crop_center.progressive.jpg?v=1673638271" />
+            </Card>
+            </Col>
+
+            <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://cdn.shopify.com/s/files/1/0057/3728/3618/products/wandavision.R85648_240x360_crop_center.progressive.jpg?v=1673638271" />
+            </Card>
+            </Col>
+
+            <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://cdn.shopify.com/s/files/1/0057/3728/3618/products/wandavision.R85648_240x360_crop_center.progressive.jpg?v=1673638271" />
+            </Card>
+            </Col>
+
+            <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://cdn.shopify.com/s/files/1/0057/3728/3618/products/wandavision.R85648_240x360_crop_center.progressive.jpg?v=1673638271" />
+            </Card>
+            </Col>
+
+            <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://cdn.shopify.com/s/files/1/0057/3728/3618/products/wandavision.R85648_240x360_crop_center.progressive.jpg?v=1673638271" />
+            </Card>
+            </Col>
+
+            <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://cdn.shopify.com/s/files/1/0057/3728/3618/products/wandavision.R85648_240x360_crop_center.progressive.jpg?v=1673638271" />
+            </Card>
+            </Col>
+
+            <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://cdn.shopify.com/s/files/1/0057/3728/3618/products/wandavision.R85648_240x360_crop_center.progressive.jpg?v=1673638271" />
+            </Card>
+            </Col>
+
+            <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://cdn.shopify.com/s/files/1/0057/3728/3618/products/wandavision.R85648_240x360_crop_center.progressive.jpg?v=1673638271" />
+            </Card>
+            </Col>
+
+            <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://cdn.shopify.com/s/files/1/0057/3728/3618/products/wandavision.R85648_240x360_crop_center.progressive.jpg?v=1673638271" />
+            </Card>
+            </Col>
+
+            <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://cdn.shopify.com/s/files/1/0057/3728/3618/products/wandavision.R85648_240x360_crop_center.progressive.jpg?v=1673638271" />
+            </Card>
+            </Col>
+
+            <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://cdn.shopify.com/s/files/1/0057/3728/3618/products/wandavision.R85648_240x360_crop_center.progressive.jpg?v=1673638271" />
+            </Card>
+            </Col>
+
+            <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://cdn.shopify.com/s/files/1/0057/3728/3618/products/wandavision.R85648_240x360_crop_center.progressive.jpg?v=1673638271" />
+            </Card>
+            </Col>
+
+            <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://cdn.shopify.com/s/files/1/0057/3728/3618/products/wandavision.R85648_240x360_crop_center.progressive.jpg?v=1673638271" />
+            </Card>
+            </Col>
+
+            <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://cdn.shopify.com/s/files/1/0057/3728/3618/products/wandavision.R85648_240x360_crop_center.progressive.jpg?v=1673638271" />
+            </Card>
+            </Col>
+
+            <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://cdn.shopify.com/s/files/1/0057/3728/3618/products/wandavision.R85648_240x360_crop_center.progressive.jpg?v=1673638271" />
+            </Card>
+            </Col>
+
+            <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://cdn.shopify.com/s/files/1/0057/3728/3618/products/wandavision.R85648_240x360_crop_center.progressive.jpg?v=1673638271" />
+            </Card>
+            </Col>
+
+            <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://cdn.shopify.com/s/files/1/0057/3728/3618/products/wandavision.R85648_240x360_crop_center.progressive.jpg?v=1673638271" />
+            </Card>
+            </Col>
+
+
+            <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://cdn.shopify.com/s/files/1/0057/3728/3618/products/wandavision.R85648_240x360_crop_center.progressive.jpg?v=1673638271" />
+            </Card>
+            </Col>
+
+            <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://cdn.shopify.com/s/files/1/0057/3728/3618/products/wandavision.R85648_240x360_crop_center.progressive.jpg?v=1673638271" />
+            </Card>
+            </Col>
+
+            <Col> 
+        <Card style={{ width: "10rem"}}>
+              <Card.Img  variant="top" src="https://cdn.shopify.com/s/files/1/0057/3728/3618/products/wandavision.R85648_240x360_crop_center.progressive.jpg?v=1673638271" />
+            </Card>
+            </Col>
       </Row>
        
     </div>
